a67c46c93514c3700157a9f7ecbcf392213c088c
Kenrick Wu <kenrick.wu@gmail.com>
2016-01-11 23:37:27 +0800

    Adding the react component sub-generator.

---
+++ generators/comp/index.js
@@ -0,0 +1,25 @@
+var generators = require("yeoman-generator"),
+    _ = require("lodash");
+
+module.exports = generators.NamedBase.extend({
+
+    constructor: function() {
+        generators.NamedBase.apply(this, arguments);
+    },
+
+     writing : {
+         component: function() {
+             this.fs.copyTpl(
+                 this.templatePath("component.js"),
+                 this.destinationPath("src//" + this.name + "/" + this.name + ".js"), {
+                     componentName: this._capitalizeFirstLetter(this.name)
+                 }
+             );
+         },
+     
+     },   
+
+     _capitalizeFirstLetter: function (str) {
+        return str.charAt(0).toUpperCase() + str.slice(1);
+     }
+});
---
+++ generators/comp/templates/component.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default class <%= componentName %> extends React.Component {
+
+    constructor(pros) {
+        super(pros);
+    }
+
+    render() {
+
+        return (
+            
+        );
+    }
+}
