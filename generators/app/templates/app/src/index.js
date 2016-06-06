9ff53c479410e31928b24298c0b8fa0fb2afb98c
Kenrick Wu <kenrick.wu@gmail.com>
2016-06-06 20:01:05 +0800

    Fixed configuration files and added minor css for box-sizing

--- generators/app/templates/app/src/index.js
+++ generators/app/templates/app/src/index.js
@@ -1,44 +1,43 @@
-import React from "react";
+import React, { PropTypes } from "react";
 import { render } from "react-dom";
 import { Router, Route, Link, browserHistory } from "react-router";
 
 import Home from "./home/home";
 import About from "./about/about";
 
-require("../styles/layout");
 require("../styles/site");
 
 export default class Index extends React.Component {
 
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
+  render() {
 
-        return (
-            <div>
-                <header>
-                    <img className="logo" src="/images/reactlogo.png" alt="react's logo"/>
-                    <ul className="nav-list">
-                        <li> <Link to="/home">Home</Link> </li>
-                        <li> <Link to="/about">About</Link> </li>
-                    </ul>
-                </header>
-                <div clasName="main">{this.props.children}</div>
-            </div>
-        );
-    }
+    return (
+      <div>
+        <header>
+          <img className="logo" src="/images/reactlogo.png" alt="react's logo"/>
+          <ul className="nav-list">
+            <li> <Link to="/home">Home</Link> </li>
+            <li> <Link to="/about">About</Link> </li>
+          </ul>
+        </header>
+        <div clasName="main">{this.props.children}</div>
+      </div>
+    );
+  }
 }
 
 const routes = {
-    path: "/",
-    component: Index,
-    indexRoute: { component: Home },
-    childRoutes: [
-        { path: "home", component: Home },
-        { path: "about", component: About}
-    ]
+  path: "/",
+  component: Index,
+  indexRoute: { component: Home },
+  childRoutes: [
+    { path: "home", component: Home },
+    { path: "about", component: About}
+  ]
 };
 
 render(<Router routes={routes} />, document.getElementById("container"));
